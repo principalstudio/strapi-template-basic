cdc74e49303b93ba68fb4660c86eb139ee048aa8
Jules Renaud <renaud.jules@gmail.com>
2021-08-04 14:44:45 -0400

    Slight change to ADMIN_URL prod setup

--- template/scripts/files/config/env/production/server.js
+++ template/scripts/files/config/env/production/server.js
@@ -3,7 +3,8 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   url: env('URL', null),
   admin: {
-    url: env('ADMIN_URL', '/admin'),
+    // TODO: run command bellow
+    url: '/<run `openssl rand 10 -hex` until the string starts with a letter>',
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
