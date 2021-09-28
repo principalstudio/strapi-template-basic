8b29100744f87f146a8324ad3ed636c3c64ee2b6
Jules Renaud <renaud.jules@gmail.com>
2021-09-28 17:29:12 -0400

    Apply fix to functions/override-slug

--- template/config/functions/override-slug.js
+++ template/config/functions/override-slug.js
@@ -5,22 +5,37 @@ const slugify = require('slugify');
 /**
  * Override slug with slugified string
  *
+ * Usage (in model .js file):
+ *
+ * lifecycles: {
+ *   async beforeCreate(data) {
+ *     data = await strapi.config.functions['override-slug'](data, 'page');
+ *   },
+ *   async beforeUpdate(params, data) {
+ *     data = await strapi.config.functions['override-slug'](data, 'page', params);
+ *   },
+ * },
+ *
  * @param {Object} data entity data
  * @param {String} model Strapi model name
+ * @param {?Object} params strapi query params
  * @param {?String} sourceStr String to slugify, if null data.title is used
  * @param {?String} extraFieldKey Key of an extra field to be set with value to be slugified
  */
 module.exports = async (
   data,
   model,
+  params = null,
   sourceStr = null,
   extraFieldKey = null
 ) => {
   const isPublishing =
     data.hasOwnProperty('published_at') && Object.keys(data).length === 1;
 
+  // bail if entity is being puclished or unpublished
   if (isPublishing) return;
 
+  const id = params && params.id ? params.id : data.id;
   const toSlugify = sourceStr ? sourceStr : data.title;
   let slug = slugify(toSlugify, {
     lower: true,
@@ -28,8 +43,8 @@ module.exports = async (
   });
   const countParams = { slug };
 
-  if (data.id) {
-    countParams.id_nin = [data.id];
+  if (id) {
+    countParams.id_nin = [id];
   }
 
   const slugCount = await strapi.query(model).count(countParams);
