d5e321e2254739fb7557415f2230ee5314019124
Jules Renaud <renaud.jules@gmail.com>
2021-11-04 16:26:23 -0400

    Bump version number

--- template.js
+++ template.js
@@ -2,7 +2,7 @@ module.exports = function(scope) {
   return {
     package: {
       config: {
-        'principal-strapi-template-basic-version': '2.2.2'
+        'principal-strapi-template-basic-version': '2.3.0'
       },
       dependencies: {
         'pg': 'latest',
